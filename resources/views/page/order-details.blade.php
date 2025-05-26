69f43d500f5e6b61c40d48913aef3442a36c611c
Bandsuu <mjpbandala@gmail.com>
2025-05-26 22:49:25 +0800

    wag ka na magloko pls

--- resources/views/page/order-details.blade.php
+++ resources/views/page/order-details.blade.php
@@ -31,7 +31,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Order Details - Imajica Booking System</title>
+    <title>Order Details</title>
 
     
       <meta name="description" content="View order details in the Imajica Booking System" />
